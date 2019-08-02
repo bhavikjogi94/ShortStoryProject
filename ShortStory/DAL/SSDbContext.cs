b3052aa45caf7446fce7d21f86cd0741077451de
Bhavik Jogi <jogi.net7@gmail.com>
2019-08-02 11:50:42 -0400

    Created Migration files for Database

---
+++ ShortStory/DAL/SSDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using ShortStoryBOL;
+using System;
+
+namespace DAL
+{
+    public class SSDbContext : IdentityDbContext
+
+
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionBuilder)
+        {
+            base.OnConfiguring(optionBuilder);
+            optionBuilder.UseSqlServer(@"Server=CATO-BJOGI\SQLEXPRESS;Database=SSAngular8Db;Trusted_Connection=True;");
+        }
+
+        public DbSet<Story> Stories { get; set; }
+    }
+}
