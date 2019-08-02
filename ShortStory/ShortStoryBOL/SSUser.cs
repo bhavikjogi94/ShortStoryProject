4aad592b164a32871d9acf4844fa036b04a90e3c
Bhavik Jogi <jogi.net7@gmail.com>
2019-08-02 10:21:05 -0400

    Created Story table class and SSUser table class for Database

---
+++ ShortStory/ShortStoryBOL/SSUser.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace ShortStoryBOL
+{
+    [Table("SSUser")]
+    public class SSUser : IdentityUser
+    {
+        [Required]
+        public DateTime Date { get; set; }
+
+        public string ProfilePicPath { get; set; }
+
+        public IEnumerable<Story> Stories { get; set; }
+
+    }
+}
